f8247ac3c75c8af5020ebb2f77ebe3fd70b60836
jacobbergendorff <jacob_bergendorff@hotmail.com>
2023-01-05 10:07:34 +0100

    added functions for footer and header for all html files nav bar and footer design still in progress

--- website/scripts/wp-script.js
+++ website/scripts/wp-script.js
@@ -16,3 +16,19 @@ for (let i = 0; i < textDivs.length; i++) {
         }
     });
 }
+
+// Function that enables for clicking the nav buttons
+const navButtons = document.querySelectorAll("li");
+
+navButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        button.classList.toggle("selected");
+    });
+});
+
+// Function that displays footer when the user scrolls to the bottom
+window.onscroll = function() {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+      document.querySelector("footer").style.display = "block";
+    }
+  };
