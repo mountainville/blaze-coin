b49dc24c6e207e7b5e89ba03023859e84a60d7ce
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2023-01-05 12:34:00 +0100

    updated css

--- website/scripts/index-script.js
+++ website/scripts/index-script.js
@@ -1,16 +1,30 @@
-
 // Function that enables for clicking the nav buttons
 const navButtons = document.querySelectorAll("li");
 
 navButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        button.classList.toggle("selected");
-    });
+  button.addEventListener("click", () => {
+    button.classList.toggle("selected");
+  });
 });
 
 // Function that displays footer when the user scrolls to the bottom
-window.onscroll = function() {
-    if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-      document.querySelector("footer").style.display = "block";
+window.onscroll = function () {
+  if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+    document.querySelector("footer").style.display = "block";
+  }
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("ix-show");
+      entry.target.classList.remove("ix-translateX");
+      entry.target.classList.remove("ix-translateY");
+    } else {
+      entry.target.classList.remove("ix-show");
     }
-  };
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".ix-hidden");
+hiddenElements.forEach((el) => observer.observe(el));
