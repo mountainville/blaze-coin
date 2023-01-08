d05263c1362d44f11e54662a3b3e7f202fd0ca75
FredrikCarlssn <113025364+FredrikCarlssn@users.noreply.github.com>
2023-01-08 19:53:51 +0100

    Updated styling on website

--- website/scripts/team-script.js
+++ website/scripts/team-script.js
@@ -2,14 +2,30 @@
 const navButtons = document.querySelectorAll("li");
 
 navButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        button.classList.toggle("selected");
-    });
+  button.addEventListener("click", () => {
+    button.classList.toggle("selected");
+  });
 });
 
 // Function that displays footer when the user scrolls to the bottom
-window.onscroll = function() {
-    if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-      document.querySelector("footer").style.display = "block";
+window.onscroll = function () {
+  if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+    document.querySelector("footer").style.display = "block";
+  }
+};
+
+// Function for animation of website. Observes if object is displayed or not
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("tp-show");
+      entry.target.classList.remove("tp-translateX");
+      entry.target.classList.remove("tp-translateY");
+    } else {
+      entry.target.classList.remove("tp-show");
     }
-  };
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".tp-hidden");
+hiddenElements.forEach((el) => observer.observe(el));
