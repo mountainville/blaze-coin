c0889f9be17491270ecf5228b9b2c17f5453c384
jacobbergendorff <jacob_bergendorff@hotmail.com>
2023-01-12 13:53:40 +0100

    .added basic files and wrote button function

---
+++ website/scripts/lending-script.js
@@ -0,0 +1,8 @@
+const lendingButton = document.getElementById('lending-button');
+const lendingForm = document.getElementById('lending-form');
+
+lendingButton.addEventListener('click', function() {
+    // retrieve form data 
+    
+});
+    // submit form
